a3c4d5594c2eee3576faaf62aa90b98b2504c963
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-09-04 12:00:28 +0100

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,8 @@ require("@nomiclabs/hardhat-ethers");
 
 const {
 	ALCHEMY_API_URL,
+	//Add Polygon Mumbai URL
+	ALCHEMY_API_URL_MUMBAI,
 	PRIVATE_KEY
 } = process.env;
 
@@ -13,6 +15,11 @@ module.exports = {
 		goerli: {
 			url: ALCHEMY_API_URL,
 			accounts: [`0x${PRIVATE_KEY}`]
-		}
+		},
+		// Add Polygon Mumbai network
+		Mumbai: {
+			url: ALCHEMY_API_URL_MUMBAI,
+			accounts: [`0x${PRIVATE_KEY}`]
+		},
 	},
 }
